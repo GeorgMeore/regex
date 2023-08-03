b5b77a2a30a158c2c73f7f641274078abde4815a
ggp <>
2023-08-03 20:59:23 +0300

    add some unit tests

---
+++ parse_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+type testCase struct {
+	in  string
+	out string
+	err string
+}
+
+var cases = []testCase{
+	{in: "a(b|c)*d?", out: "concat{a,concat{star{alt{b,c}},opt{d}}}"},
+	{in: "a(b?|c?)cd", out: "concat{a,concat{alt{opt{b},opt{c}},concat{c,d}}}"},
+	{in: ")abc", err: "wanted: a letter or '(', got: ')'"},
+	{in: "", err: "wanted: a letter or '(', got: EOF"},
+	{in: "?", err: "wanted: a letter or '(', got: '?'"},
+	{in: "(*)", err: "wanted: a letter or '(', got: '*'"},
+	{in: "a(bc", err: "wanted: ')', got: EOF"},
+	{in: "((a)))", err: "wanted: EOF, got: ')'"},
+}
+
+func TestParse(t *testing.T) {
+	for _, c := range cases {
+		res, err := parse(c.in)
+		if err != nil && err.Error() != c.err {
+			t.Errorf("bad error message: expected %s, got %s", c.err, err.Error())
+		}
+		if res != nil && res.String() != c.out {
+			t.Errorf("bad value: expected %s, got %s", c.out, res.String())
+		}
+	}
+}
